00f3c344fd9423ba8c24298f547415a38c3f7f0c
Namelessfury <69160890+Namelessfury@users.noreply.github.com>
2022-08-03 13:23:36 -0400

    Update default.php

--- admin/tmpl/reports/default.php
+++ admin/tmpl/reports/default.php
@@ -1,4 +1,45 @@
 <?php
+
 defined('_JEXEC') or die('Restricted Access');
+
+use Joomla\CMS\Factory;
+
 ?>
 
+<html>
+    <body>
+
+        <h2>Delete Reports</h2>
+
+        <p>This button will delete ALL reports in the system.</p>
+        <p>BE VERY CAREFUL!</p>
+
+        <script>
+            function deleteReports() {
+            let text = "Are you sure you want to delete all the reports?\nEither OK or Cancel.";
+            
+            if (confirm(text) == true)
+            {
+                <?php
+                    $db = JFactory::getDbo();
+                    $query = $db->getQuery(true);
+                    $query = "DELETE FROM reports";
+                    $db->setQuery($query)->execute();
+                ?>
+
+                text = "All reports have been deleted.";
+            }
+            else
+            {
+                text = "You canceled.";
+            }
+            document.getElementById("response").innerHTML = text;
+            }
+        </script>
+
+        <button class="btn btn-danger" onclick="deleteReports()">Delete All Reports</button>
+
+        <p id="response"></p>
+
+    </body>
+</html>
