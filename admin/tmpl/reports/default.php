b6d39df641526d8d2f6dc3ef2dbdcccadf7c9fae
Namelessfury <69160890+Namelessfury@users.noreply.github.com>
2022-08-10 00:56:31 -0400

    Working + Checkbox
    Backend delete table button now works. 2 button confirm to delete table

--- admin/tmpl/reports/default.php
+++ admin/tmpl/reports/default.php
@@ -4,42 +4,34 @@ defined('_JEXEC') or die('Restricted Access');
 
 use Joomla\CMS\Factory;
 
+//deletes all reports when the red button is pressed 
+if(isset($_POST['deleteReports']))
+{
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
+    $query = "DELETE FROM reports";
+    $db->setQuery($query)->execute();
+    echo 'The reports have been deleted';
+}
 ?>
 
 <html>
     <body>
-
+        <br><br>
         <h2>Delete Reports</h2>
 
-        <p>This button will delete ALL reports in the system.</p>
+        <p>This button will <b>delete all reports</b> currently in the system</p>
         <p>BE VERY CAREFUL!</p>
+        
+        <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" id="checkboxConfirm" onchange="document.getElementById('deleteReports').disabled = !this.checked;">
+            <label class="form-check-label" for="checkboxConfirm">Yes, I want to delete all reports</label>
+        </div>
 
-        <script>
-            function deleteReports() {
-            let text = "Are you sure you want to delete all the reports?\nEither OK or Cancel.";
-            
-            if (confirm(text) == true)
-            {
-                <?php
-                    $db = JFactory::getDbo();
-                    $query = $db->getQuery(true);
-                    $query = "DELETE FROM reports";
-                    $db->setQuery($query)->execute();
-                ?>
+        <br>
 
-                text = "All reports have been deleted.";
-            }
-            else
-            {
-                text = "You canceled.";
-            }
-            document.getElementById("response").innerHTML = text;
-            }
-        </script>
-
-        <button class="btn btn-danger" onclick="deleteReports()">Delete All Reports</button>
-
-        <p id="response"></p>
-
+        <form method="post">
+            <input class="btn btn-danger" type="submit" name="deleteReports" id="deleteReports" value="Delete All Reports" disabled/>
+        </form>
     </body>
 </html>
